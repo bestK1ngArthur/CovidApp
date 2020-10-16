0805fc199c098a547119bb4953497c51a4592329
Artem Belkov <bestk1ngarthur@aol.com>
2020-10-16 18:02:08 +0300

    Initial Commit

---
+++ Covid19/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Covid19
+//
+//  Created by Artem Belkov on 16.10.2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Covid19/Covid19App.swift
@@ -0,0 +1,17 @@
+//
+//  Covid19App.swift
+//  Covid19
+//
+//  Created by Artem Belkov on 16.10.2020.
+//
+
+import SwiftUI
+
+@main
+struct Covid19App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
